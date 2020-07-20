120b727ab54cd0590c0581c3601d7f982f7adfd4
archeapxa <60774969+archeapxa@users.noreply.github.com>
2020-07-20 14:56:04 +0300

    slider added

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+
+  // fade effect
+  // effect: "fade",
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+
+})
